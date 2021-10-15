15c4c99b825019ce1a20410c6e57688fa03a2eed
Grzegorz Motyl <motylg01@stepstone.com>
2021-10-15 11:46:44 +0200

    compose #4

--- src/compose.ts
+++ src/compose.ts
@@ -25,7 +25,7 @@ export const lastNote = (students: Student[]) => {
 // # to retrieve first student's name
 // #########################################################################
 
-export const firstStudentName = (students: Student[]): string => "Ala";
+export const firstStudentName = (students: Student[]): string => "Ala Makota";
 
 // #########################################################################
 // # 3
@@ -42,3 +42,17 @@ export var averageNote = function (students: Student[]) {
   }, students);
   return _average(notes);
 };
+
+// #########################################################################
+// # 4
+// #########################################################################
+// # unskip test '#4 sanitizeNames' in __tests__/compose.tets.ts
+// #########################################################################
+// # Write a function: sanitizeNames() using compose that returns
+// # a list of lowercase and underscored names:
+// # e.g: sanitizeNames(["Hello World"]) //=> ["hello_world"].
+// #########################################################################
+
+const _underscore = R.replace(/\W+/g, "_"); //<-- leave this alone and use to sanitize
+
+export const sanitizeNames = undefined;
