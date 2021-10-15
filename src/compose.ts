480a82b7d56d701b6b1b7033ffbd98e78bf80699
Grzegorz Motyl <motylg01@stepstone.com>
2021-10-15 10:58:14 +0200

    compose #3

--- src/compose.ts
+++ src/compose.ts
@@ -26,3 +26,19 @@ export const lastNote = (students: Student[]) => {
 // #########################################################################
 
 export const firstStudentName: Student = undefined;
+
+// #########################################################################
+// # 3
+// #########################################################################
+// # Use the helper function _average to refactor averageNote as a composition
+// #########################################################################
+const _average = function (xs) {
+  return R.reduce(R.add, 0, xs) / xs.length;
+}; // <- leave be
+
+var averageNote = function (cars) {
+  var dollar_values = R.map(function (c) {
+    return c.dollar_value;
+  }, cars);
+  return _average(dollar_values);
+};
