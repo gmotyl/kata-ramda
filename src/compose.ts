faa5f035b1e32044f76ced1a65fcd7691551207a
Grzegorz Motyl <motylg01@stepstone.com>
2021-10-15 11:57:05 +0200

    rebase

--- src/compose.ts
+++ src/compose.ts
@@ -7,16 +7,15 @@ import R from "ramda";
 // # Hint: Ramda functions are curried.
 // #########################################################################
 
-export type Student = {
+type Student = {
   name: string;
   note: number;
 };
 
-export const lastNote = (students: Student[]) => {
-  const lastElement = R.last(students);
-
-  return R.prop("note", lastElement);
-};
+export const lastNote = R.compose<Student[], Student, number>(
+  R.prop("note"),
+  R.last
+);
 
 // #########################################################################
 // # 2
@@ -25,7 +24,11 @@ export const lastNote = (students: Student[]) => {
 // # to retrieve first student's name
 // #########################################################################
 
-export const firstStudentName = (students: Student[]): string => "Ala Makota";
+// export const firstStudentName = (students: Student[]): string => "Ala";
+export const firstStudentName = R.compose<Student[], Student, string>(
+  R.prop("name"),
+  R.head
+);
 
 // #########################################################################
 // # 3
@@ -33,15 +36,13 @@ export const firstStudentName = (students: Student[]): string => "Ala Makota";
 // # Use the helper function _average to refactor averageNote as a composition
 // #########################################################################
 const _average = function (xs: number[]) {
-  return R.reduce<number, number>(R.add, 0, xs) / xs.length;
-}; // <- leave be
-
-export var averageNote = function (students: Student[]) {
-  var notes = R.map(function (s) {
-    return s.note;
-  }, students);
-  return _average(notes);
+  return R.reduce<number, number>(R.add, 0, xs) / xs.length; // <- leave be
 };
+
+export var averageNote = R.compose<Student[], number[], number>(
+  _average,
+  R.map(R.prop("note"))
+);
 
 // #########################################################################
 // # 4
@@ -55,4 +56,8 @@ export var averageNote = function (students: Student[]) {
 
 const _underscore = R.replace(/\W+/g, "_"); //<-- leave this alone and use to sanitize
 
-export const sanitizeNames = undefined;
+export const sanitizeNames = R.compose(
+  R.map(_underscore),
+  R.map(R.toLower),
+  R.map(R.prop("name"))
+);
