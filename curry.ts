06b5c25ef6a5d146d79047086b214d3ee3644f16
Grzegorz Motyl <motylg01@stepstone.com>
2021-10-18 17:13:58 +0200

    curry #1

---
+++ curry.ts
@@ -0,0 +1,8 @@
+// #########################################################################
+// # 1
+// #########################################################################
+// # implement 'curry' function that takes as parameter function "f" with two parameters
+// # and returns a function which takes two parameters one at a time
+// #########################################################################
+
+export const curry = undefined;
