403989d5c35fca26881fd02204bc269359fed04c
Grzegorz Motyl <motylg01@stepstone.com>
2021-11-15 13:35:41 +0100

    #3 curry

--- src/__tests__/curry.test.ts
+++ src/__tests__/curry.test.ts
@@ -1,7 +1,7 @@
-import { curry, getOdds } from "../curry";
+import { curry, filterQs, getOdds, sentences, words } from "../curry";
 
 describe("curry", () => {
-  it.skip("should curry two params", () => {
+  it.skip("#1 should curry two params", () => {
     const add = (x, y) => x + y;
     const curriedAdd = curry(add);
     const add2 = curriedAdd(2);
@@ -11,7 +11,26 @@ describe("curry", () => {
     expect(add7(100)).toBe(107);
   });
 
-  it.skip("shuld return odd numbers", () => {
+  it.skip("#2 shuld return odd numbers", () => {
     expect(getOdds([1, 2, 3, 4, 5])).toEqual([1, 3, 5]);
+  });
+
+  it("#3 split", () => {
+    expect(words("Ala ma kota")).toEqual(["Ala", "ma", "kota"]);
+  });
+
+  it("#4 sentences", () => {
+    expect(sentences(["Ala ma kota", "kot ma Alę"])).toEqual([
+      ["Ala", "ma", "kota"],
+      ["kot", "ma", "Alę"],
+    ]);
+  });
+
+  it("#5 filterQs", () => {
+    expect(filterQs(["quick", "camels", "quarry", "over", "quails"])).toEqual([
+      "quick",
+      "quarry",
+      "quails",
+    ]);
   });
 });
--- src/curry.ts
+++ src/curry.ts
@@ -22,3 +22,38 @@ const isOdd = modulo(2);
 const filter = R.curry((xs, f) => xs.filter(f));
 
 export const getOdds = filter(isOdd);
+
+// #########################################################################
+// # 3
+// #########################################################################
+// # refactor words
+// # leverage fact that split is curried
+// #########################################################################
+
+const split = R.curry((delimiter: string, str: string) => str.split(delimiter));
+
+export const words = function (str: string) {
+  return split(" ", str);
+};
+
+// #########################################################################
+// # 4
+// #########################################################################
+// # sentences use map to make a new words fn that not only works on 1 string,
+// # but on an array of strings.
+// # refactor it to simpler form and notice inferred typings
+// #########################################################################
+
+export const sentences = (xs: string[]) => R.map(words, xs);
+
+// #########################################################################
+// # 5
+// #########################################################################
+// # refactor fiterQs to remove noise
+// #########################################################################
+
+export const filterQs = function (xs: string[]) {
+  return R.filter(function (x) {
+    return R.test(/q/gi, x);
+  }, xs);
+};
